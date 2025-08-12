8de5efb939c615cf5846c21c552f47f3b2fcbdb2
ubicoders <ubicoders.0@gmail.com>
2025-08-12 13:04:13 -0700

    dd

--- buildinorder.bash
+++ buildinorder.bash
@@ -13,9 +13,17 @@ docker compose build ubicoders_ros2_humble
 docker compose build ubicoders_ros2_humble_px4
 docker compose build ubicoders_ros2_humble_px4uxrcedds
 
+
+# CUDA based ubuntu ========================================
+docker compose build ubicoders_u24_cuda12
+docker compose build ubicoders_u24_cuda12_zed
+
 # CUDA based ROS2 ========================================
 docker compose build ubicoders_ros2_humble_cuda12_ocv
 docker compose build ubicoders_ros2_humble_cuda12_ocv_zed
+
+
+
 
 # PUSH all
 docker push ubicoders/px4:v1.14
@@ -27,3 +35,7 @@ docker push ubicoders/ros2:humble_px4
 docker push ubicoders/ros2:humble_px4_uxrcedds
 docker push ubicoders/ros2:humble_cuda12_ocv
 docker push ubicoders/ros2:humble_cuda12_ocv_zed
+
+docker push ubicoders/ubuntu:u24_cuda12
+docker push ubicoders/ubuntu:u24_cuda12_zed
+
