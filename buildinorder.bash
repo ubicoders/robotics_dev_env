68ece24768c1c7d42784b2abad93a4b69f0db054
ubicoders <ubicoders.0@gmail.com>
2025-11-20 10:53:03 -0800

    updated

--- buildinorder.bash
+++ buildinorder.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# U24 PX4 ===========
+docker compose build ubicoders_u24_px4
+
 # ROS2 ====================================================
 docker compose build ubicoders_ros2_jazzy
 docker compose build ubicoders_ros2_jazzy_px4
@@ -7,19 +10,21 @@ docker compose build ubicoders_ros2_jazzy_px4uxrcedds
 
 # CUDA based ubuntu ========================================
 docker compose build ubicoders_u24_cuda12
-docker compose build ubicoders_u24_cuda12_zed
+docker compose build ubicoders_u24_cuda12_ocv_zed
 
 # CUDA based ROS2 ========================================
-docker compose build ubicoders_ros2_humble_cuda12_ocv
-docker compose build ubicoders_ros2_humble_cuda12_ocv_zed
+docker compose build ubicoders_u24_cuda12_jazzy
 
 
 # PUSH all
+docker push ubicoders/px4:v1.16
+
 docker push ubicoders/ros2:jazzy
 docker push ubicoders/ros2:jazzy_px4
 docker push ubicoders/ros2:jazzy_px4_uxrcedds
-# docker push ubicoders/ros2:jazzy_cuda12_ocv
-# docker push ubicoders/ros2:jazzy_cuda12_ocv_zed
+docker push ubicoders/ros2:jazzy_cuda12
+
 docker push ubicoders/ubuntu:u24_cuda12
-docker push ubicoders/ubuntu:u24_cuda12_zed
+docker push ubicoders/ubuntu:u24_cuda12_ocv_zed
+
 
