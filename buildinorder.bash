26715b2a6c005d305c929026f40f7bf127d47bcc
ubicoders <ubicoders.0@gmail.com>
2025-11-19 20:06:30 -0800

    updated

--- buildinorder.bash
+++ buildinorder.bash
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# PX4 only U20 ===========================================
-docker compose build ubicoders_u20_px4
-
-# ROS1 ====================================================
-docker compose build ubicoders_ros1_noetic
-docker compose build ubicoders_ros1_noetic_px4
-docker compose build ubicoders_ros1_noetic_px4_mavros
-
 # ROS2 ====================================================
-docker compose build ubicoders_ros2_humble
-docker compose build ubicoders_ros2_humble_px4
-docker compose build ubicoders_ros2_humble_px4uxrcedds
-
+docker compose build ubicoders_ros2_jazzy
+docker compose build ubicoders_ros2_jazzy_px4
+docker compose build ubicoders_ros2_jazzy_px4uxrcedds
 
 # CUDA based ubuntu ========================================
 docker compose build ubicoders_u24_cuda12
@@ -23,19 +14,12 @@ docker compose build ubicoders_ros2_humble_cuda12_ocv
 docker compose build ubicoders_ros2_humble_cuda12_ocv_zed
 
 
-
-
 # PUSH all
-docker push ubicoders/px4:v1.14
-docker push ubicoders/ros1:noetic
-docker push ubicoders/ros1:noetic_px4
-docker push ubicoders/ros1:noetic_px4_mavros
-docker push ubicoders/ros2:humble
-docker push ubicoders/ros2:humble_px4
-docker push ubicoders/ros2:humble_px4_uxrcedds
-docker push ubicoders/ros2:humble_cuda12_ocv
-docker push ubicoders/ros2:humble_cuda12_ocv_zed
-
+docker push ubicoders/ros2:jazzy
+docker push ubicoders/ros2:jazzy_px4
+docker push ubicoders/ros2:jazzy_px4_uxrcedds
+docker push ubicoders/ros2:jazzy_cuda12_ocv
+docker push ubicoders/ros2:jazzy_cuda12_ocv_zed
 docker push ubicoders/ubuntu:u24_cuda12
 docker push ubicoders/ubuntu:u24_cuda12_zed
 
